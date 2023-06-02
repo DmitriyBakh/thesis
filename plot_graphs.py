4e20a3f59625cb03365340ab84da570a878918a6
Dmitrii <dmitrnv@gmail.com>
2023-06-02 16:49:26 +0300

    fixup

--- plot_graphs.py
+++ plot_graphs.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
-with open('results_including_1-37214_v2.pkl', 'rb') as fp:
+with open('results.pkl', 'rb') as fp:
     results = pickle.load(fp)
 
 X = list(results['train'][0].keys())
@@ -12,6 +12,7 @@ plt.plot(X, Y)
 plt.title("Converges in overparameterization net with zero fakes samples")
 plt.xlabel("Number of the parameters")
 plt.ylabel("Iterations")
+plt.savefig('converges_num_params-iteraions.png', dpi=200)
 plt.show()
 
 
@@ -28,6 +29,7 @@ for k, v in results['train'].items():
     plt.plot(X, Y, label=f'Fake probability: {k*10}')    
 
 plt.legend()
+plt.savefig('train_num_params-iterations.png', dpi=200)
 plt.show()
 
 
@@ -45,6 +47,7 @@ for fake_prob in fake_probs:
     plt.plot(X, Y, label=f'Fake probability: {fake_prob * 10}')
 
 plt.legend()
+plt.savefig('test_loss.png', dpi=200)
 plt.show()
 
 
@@ -62,4 +65,36 @@ for fake_prob in fake_probs:
     plt.plot(X, Y, label=f'Fake probability: {fake_prob * 10}')
 
 plt.legend()
+plt.savefig('test_accuracy.png', dpi=200)
+plt.show()
+
+
+fake_probs = list(results['train'].keys())
+num_params = list(results['train'][0].keys())
+num_params = [num_params[0], num_params[7], num_params[13]]
+
+for prob in fake_probs:
+    for num_param in num_params:
+        plt.title(f"Mu, of fake probability:{prob * 10}, parameters number: {num_param}")
+        plt.xlabel("Mu")
+        plt.ylabel("Loss")
+
+        X, Y = [], []
+        for epoch in range(len(results['train'][prob][num_param])):
+            X.append(results['train'][prob][num_param][epoch]['mu_boundary'])
+            Y.append(results['train'][prob][num_param][epoch]['loss'])
+        
+        plt.plot(X, Y)
+        plt.savefig(f'mu_fake_prob-{prob*10}_num_params-{num_param}.png', dpi=200)
+        plt.show()
+
+
+with open('experiment_2_b.pkl', 'rb') as fp:
+    results = pickle.load(fp)
+
+plt.title("Test Trainig iterations over test accuracy")
+plt.xlabel("Test accuracy, %")
+plt.ylabel("Training iterations")
+plt.plot(results['accuracies'], results['epoch_counts'], 'o')
+plt.savefig('test_train_iterations.png', dpi=200)
 plt.show()
